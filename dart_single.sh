cb753fec37ba51a95a50fc1747ef9ec13efb33c6
LIU <82812565+lym-bioinfo@users.noreply.github.com>
2024-02-01 11:48:04 +0900

    Create dart_single.sh

---
+++ dart_single.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+
+# Replace these variables with your specific paths and options
+input_dir="/home/kampo314/MYC2RNAi/trimmed"
+output_dir="/home/kampo314/MYC2RNAi/mapped"
+
+# Loop through all the files in the input directory with ".fastq" extension
+for file in "$input_dir"/*.fq.gz; do
+    filename=$(basename "$file" .fq.gz)
+    dart -i /home/kampo314/RAP-DB_bwa/IRGSP-1.0_genome.fasta.gz -f "$file" -mis 1 -t 18 -bo "$output_dir/$filename.bam"
+done
