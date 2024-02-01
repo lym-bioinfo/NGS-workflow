28abccfab24947fdf0900335069172ecad9fadc3
LIU <82812565+lym-bioinfo@users.noreply.github.com>
2024-02-01 11:43:58 +0900

    Create single_end_trim.sh

---
+++ single_end_trim.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+
+input_dir="/home/kampo314/TGAP_MT"
+output_dir="/home/kampo314/TGAP_MT/trimmed"
+
+mkdir -p "$output_dir"
+
+for file in "$input_dir"/*.fastq.gz; do
+
+  filename=$(basename "$file" .fastq)
+  
+  trim_galore -stringency 3 -q 25 --length 80 -j 8 --output_dir "$output_dir" "$file"
+  
+  mv "$output_dir"/"${filename}_trimmed.fastq" "$output_dir"/"${filename}_trimmed.fastq"
+done
